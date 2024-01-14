53c0f3679a7f3e00f172808220f4cfba65415f00
ullas.jain <ullas.jain.github@gmail.com>
2024-01-14 20:07:20 +0530

    Generates mock using mockgen

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,21 +6,28 @@ import (
 	"fmt"
 )
 
-// Provides all functions to execute db queries and transactions
-type Store struct {
+// Store ...
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+// SQLStore provides all functions to execute db queries and transaction
+type SQLStore struct {
 	db *sql.DB
 	*Queries
 }
 
-// NewStore creates a new Store
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+// NewStore creates a new store
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
-		Queries: New(db), // instance is created via SQLC
+		Queries: New(db),
 	}
 }
 
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+// ExecTx executes a function within a database transaction
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
@@ -50,7 +57,7 @@ type TransferTxResult struct {
 	ToEntry     Entry    `json:"to_entry"`
 }
 
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
